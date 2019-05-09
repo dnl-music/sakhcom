720010986feebb2f8066cea5ce0ddd0aae91e1b6
Danil Kaykov <baza-nsk@ya.ru>
2019-05-09 07:15:05 +0700

    init migration

---
+++ migrations/m190509_000751_create_table_news.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190509_000751_create_table_news
+ */
+class m190509_000751_create_table_news extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('rubric', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255),
+        ]);
+
+        $this->createTable('news', [
+           'id' => $this->primaryKey(),
+           'title' => $this->string(255),
+           'short_desc' => $this->string(255),
+           'content' => $this->text(),
+           'image' => $this->string(255),
+           'rubric_id' => $this->integer()->notNull(),
+           'likes' => $this->integer()->defaultValue(0)
+        ]);
+
+        $this->addForeignKey(
+            'fk-news-rubric',
+            'news',
+            'rubric_id',
+            'rubric',
+            'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190509_000751_create_table_news cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190509_000751_create_table_news cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
